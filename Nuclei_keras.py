d66df6f79ca55f63b99ccd870886718450bc5403
juancanete86 <canete86@gmail.com>
2018-04-06 19:26:51 +0200

    Slight modification

--- Nuclei_keras.py
+++ Nuclei_keras.py
@@ -24,12 +24,12 @@ from skimage.morphology import label
 
 from keras.models import Model, load_model
 from keras.layers import Input
-from keras.layers.core import Dropout, Lambda
-from keras.layers.convolutional import Conv2D, Conv2DTranspose
-from keras.layers.pooling import MaxPooling2D
-from keras.layers.merge import concatenate
+from keras.layers.core import Lambda
+from keras.layers.convolutional import Conv2D
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import backend as K
+
+from ConvModel import ConvModel
 
 import tensorflow as tf
 #from tensorflow.python.client import device_lib
@@ -118,66 +118,22 @@ def mean_iou(y_true, y_pred):
 inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
 s = Lambda(lambda x: x / 255) (inputs)
 
-c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
-c1 = Dropout(0.1) (c1)
-c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
-p1 = MaxPooling2D((2, 2)) (c1)
+conv_model = ConvModel.ConvModel(inputs, s)
 
-c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
-c2 = Dropout(0.1) (c2)
-c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
-p2 = MaxPooling2D((2, 2)) (c2)
-
-c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
-c3 = Dropout(0.2) (c3)
-c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
-p3 = MaxPooling2D((2, 2)) (c3)
-
-c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
-c4 = Dropout(0.2) (c4)
-c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
-p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
-
-c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
-c5 = Dropout(0.3) (c5)
-c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
-
-u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
-u6 = concatenate([u6, c4])
-c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
-c6 = Dropout(0.2) (c6)
-c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
-
-u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
-u7 = concatenate([u7, c3])
-c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
-c7 = Dropout(0.2) (c7)
-c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
-
-u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
-u8 = concatenate([u8, c2])
-c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
-c8 = Dropout(0.1) (c8)
-c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
-
-u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
-u9 = concatenate([u9, c1], axis=3)
-c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
-c9 = Dropout(0.1) (c9)
-c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
-
-outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+# Ready the model
+conv_model.u_net()
+outputs = Conv2D(1, (1, 1), activation='sigmoid') (conv_model.model)
 
 model = Model(inputs=[inputs], outputs=[outputs])
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
+model.compile(optimizer='nadam', loss='binary_crossentropy', metrics=[mean_iou])
 model.summary()
 
 
 # Fit model
-earlystopper = EarlyStopping(patience=5, verbose=1)
+earlystopper = EarlyStopping(patience=7, verbose=1)
 checkpointer = ModelCheckpoint('model-dsbowl2018.h5', verbose=1, save_best_only=True)
-results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=16, epochs=150, 
-                    callbacks=[ checkpointer])
+results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=32, epochs=150, 
+                    callbacks=[earlystopper, checkpointer])
 
 # Predict on train, val and test
 model = load_model('model-dsbowl2018.h5', custom_objects={'mean_iou': mean_iou})
